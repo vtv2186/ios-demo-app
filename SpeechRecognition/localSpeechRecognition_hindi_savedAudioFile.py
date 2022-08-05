3beafb16399ee5c5463ab1b876454025e4b812b6
vtv2186 <vishnu.vithala@gmail.com>
2022-08-04 21:44:48 -0700

    Hindi XSLR with Saved Audio Fike

---
+++ SpeechRecognition/localSpeechRecognition_hindi_savedAudioFile.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  3 23:04:02 2022
+
+@author: vishnu.vithala
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  3 22:27:48 2022
+
+@author: vishnu.vithala
+"""
+
+import torch
+from torch import Tensor
+from torch.utils.mobile_optimizer import optimize_for_mobile
+import torchaudio
+from torchaudio.models.wav2vec2.utils.import_huggingface import import_huggingface_model
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+import speech_recognition as sr
+import io
+from pydub import AudioSegment
+
+
+
+tokenizer = Wav2Vec2Processor.from_pretrained('theainerd/Wav2Vec2-large-xlsr-hindi')
+
+model = Wav2Vec2ForCTC.from_pretrained('theainerd/Wav2Vec2-large-xlsr-hindi')
+
+r = sr.Recognizer()
+
+clip = AudioSegment.from_file("scent_of_a_woman_future.wav",format="wav")
+
+#clip = torchaudio.load('scent_of_a_woman_future.wav') #replace with line above
+ 
+x = torch.FloatTensor(clip.get_array_of_samples()) 
+ 
+ #data = torchaudio.load('scent_of_a_woman_future.wav')
+
+inputs = tokenizer(x, sampling_rate=16000, return_tensors='pt',padding='longest').input_values
+ 
+logits = model(inputs).logits    
+
+tokens = torch.argmax(logits,axis = -1)
+
+text = tokenizer.batch_decode(tokens) #token into a string
+ 
+print('You said:', str(text).lower())
+
+
+# with sr.Microphone(sample_rate=16000) as source :
+#     print('you can start speaking now')
+#     while True:
+#         # audio = r.listen(source)
+#         # data = io.BytesIO(audio.get_wav_data()) #arrayofbytes
+#         # clip = AudioSegment.from_file(data) #numpy array
+#         # x = torch.FloatTensor(clip.get_array_of_samples()) 
+        
+#         # inputs = tokenizer(x, sampling_rate=16000, return_tensors='pt',padding='longest').input_values #tokenizer convert tensor into way to compute
+        
+#         clip = AudioSegment.from_file("scent_of_a_woman_future.wav",format="wav")
+        
+#         x = torch.FloatTensor(clip.get_array_of_samples()) 
+        
+#         #data = torchaudio.load('scent_of_a_woman_future.wav')
+       
+#         inputs = tokenizer(x, sampling_rate=16000, return_tensors='pt',padding='longest').input_values
+        
+#         logits = model(inputs).logits    
+#         tokens = torch.argmax(logits,axis = -1)
+#         text = tokenizer.batch_decode(tokens) #token into a string
+        
+        # print('You said:', str(text).lower())
